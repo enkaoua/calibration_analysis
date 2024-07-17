7c3d7efa46dc8a7d351c81948b2ae29ee694bff3
Aure <aure.enkaoua@gmail.com>
2024-07-17 21:42:35 +0100

    feat: reprojection error for intrinsic calibration

--- charuco_utils.py
+++ charuco_utils.py
@@ -1,8 +1,193 @@
+import random
 import cv2
 import numpy as np
 import os
+import glob
+from utils import sample_dataset
 
 
+def analyse_calibration_data(board_data, 
+                             R, # reprojection df
+                             n = 1, # number of frames to use for calibration
+                             repeats = 100, # number of times to repeat the calibration process
+                             #size_chess = 15, 
+                             #calibration_save_pth = '',
+                             intrinsics_initial_guess_pth='',
+                             visualise_reprojection_error = False,
+                             waitTime=1 # wait time for when showing reprojection image
+                             ): 
+    
+    #path_to_endo_images = glob.glob(f'{data_path}/{size_chess}_charuco/pose*/acc_*_pos*_deg*_*/raw/he_calibration_images/hand_eye_endo/*.{img_ext}')
+    #path_to_rs_images = glob.glob(f'{data_path}/{size_chess}_charuco/pose*/acc_*_pos*_deg*_*/raw/he_calibration_images/hand_eye_realsense/*.{img_ext}')
+    # where to save information of aruco board
+    #charuco_board_save_pth = f'{data_path}/{size_chess}_charuco'
+    intrinsics = []
+    distortion = []
+    errors = []
+    for i in range(repeats):
+        # generate n+R random numbers that are in the range of the above loaded images
+        #random_indices = random.sample(range(len(board_data)), R+n)
+        #img_indeces_for_calibration = random_indices[R:]
+        #img_indeces_for_reprojection = random_indices[:R]
+
+        # load contents data from the table with these indeces 
+        #calibration_data = board_data.iloc[img_indeces_for_calibration]
+        #reprojection_data = board_data.iloc[img_indeces_for_reprojection]
+        calibration_data, _  = sample_dataset(board_data, total_samples=n)
+        reprojection_data = R
+        
+        # select contents of object points and image points
+        imgPoints = calibration_data.imgPoints.values
+        objPoints = calibration_data.objPoints.values
+        # load one of the images to get the shape of the image
+        image_shape = cv2.imread(calibration_data.paths.values[0]).shape[0:-1]
+        mtx, dist = calibrate_charuco_board( 
+                            intrinsics_initial_guess_pth=intrinsics_initial_guess_pth, 
+                            calibration_save_pth='',
+                            image_shape = image_shape,
+                            imgPoints=imgPoints, objPoints=objPoints,
+                            # if we want to calibrate from images
+                            #image_pths=[] ,
+                            #board = None 
+                            )
+        
+        # calculate reprojection error with these values
+        objPoints_reprojection = reprojection_data.objPoints.values
+        imgPoints_reprojection = reprojection_data.imgPoints.values
+        if visualise_reprojection_error:
+            image_paths = reprojection_data.paths.values
+        else: 
+            image_paths = None
+        err = calculate_reprojection_error(mtx, dist, objPoints_reprojection, imgPoints_reprojection, image_pths=image_paths, waitTime=waitTime)
+        print(err)
+        intrinsics.append(mtx)
+        distortion.append(dist)
+        errors.append(err)
+
+        # calculate reprojection error with these values
+        
+    #print(f'intrinsics: {intrinsics}')
+    #print(f'distortion: {distortion}')
+    
+
+    """ # split the images into the ones used for calculating calibration and ones used for reprojection
+
+
+    # load the images with these indeces from the list of images of each camera
+    img_paths_for_calibration_endo = [path_to_endo_images[i] for i in img_indeces_for_calibration]
+    img_paths_for_reprojection_rs = [path_to_rs_images[i] for i in img_indeces_for_reprojection]
+    img_paths_for_reprojection_endo = [path_to_endo_images[i] for i in img_indeces_for_reprojection]
+    img_paths_for_calibration_rs = [path_to_rs_images[i] for i in img_indeces_for_calibration]
+    
+    # create boards and 3D cube coords
+    board, number_horizontally, number_vertically, aruco_size, dictionary_id = generate_charuco_board(size_chess)
+    all3DIDs_np, allCorners3D_np, boards, number_of_corners_per_face = boards_3D_points([0], 
+                                                                                        number_horizontally,
+                                                                                        number_vertically,
+                                                                                        size_chess,
+                                                                                        aruco_size,
+                                                                                        dictionary_id, 
+                                                                                        charuco_board_save_pth=charuco_board_save_pth)
+    dictionary = cv2.aruco.getPredefinedDictionary(dictionary_id)
+    
+    camera_intrinsics_initial_guess_endo = np.loadtxt(f'{intrinsics_initial_guess_endo_pth}')
+    intrinsics_endo, distortion_endo  = calibrate_charuco_cube(boards,  dictionary,
+                                                                    number_of_corners_per_face, 
+                                                                    allCorners3D_np, 
+                                                                    all3DIDs_np, 
+                                                                    camera_intrinsics_initial_guess=camera_intrinsics_initial_guess_endo, 
+                                                                    calibration_save_pth=calibration_save_pth,
+                                                                    image_pths=img_paths_for_calibration_endo, capture_all=False )
+    
+
+    camera_intrinsics_initial_guess_rs = np.loadtxt(f'{intrinsics_initial_guess_rs_pth}')
+    intrinsics_rs, distortion_rs  = calibrate_charuco_cube(boards,
+                                                               dictionary,
+                                                                number_of_corners_per_face, 
+                                                                allCorners3D_np, 
+                                                                all3DIDs_np, 
+                                                                camera_intrinsics_initial_guess=camera_intrinsics_initial_guess_rs, 
+                                                                calibration_save_pth=calibration_save_pth,
+                                                                image_pths=img_paths_for_calibration_rs, capture_all=False )
+    
+    """
+    return errors, intrinsics, distortion
+
+
+
+def reprojection_error(imgpoints_detected, imgpoints_reprojected, image=None):
+    squared_diffs = np.square(imgpoints_detected- imgpoints_reprojected)
+    error_np = np.sqrt(np.sum(squared_diffs)/len(imgpoints_reprojected))
+
+    # different way of calculating
+    error = np.sqrt((np.square(cv2.norm(imgpoints_detected, imgpoints_reprojected, cv2.NORM_L2))/len(imgpoints_reprojected)))
+    if image is not None:
+        img_shape = image.shape
+        for corner_reprojected, corner_detected in zip(imgpoints_reprojected,imgpoints_detected):
+            # change dtypw of corner to int
+            corner_detected = corner_detected.astype(int)
+            corner_reprojected = corner_reprojected.astype(int)
+            centre_detected = corner_detected.ravel()
+            centre_reprojected = corner_reprojected.ravel()
+            # check if points are within image
+            if centre_detected[0] < 0 or centre_detected[0] > img_shape[1] or centre_detected[1] < 0 or centre_detected[1] > img_shape[0]:
+                continue
+            if centre_reprojected[0] < 0 or centre_reprojected[0] > img_shape[1] or centre_reprojected[1] < 0 or centre_reprojected[1] > img_shape[0]:
+                continue
+            cv2.circle(image, (int(centre_detected[0]),    int(centre_detected[1])),    3, (0, 0, 255), -1)
+            cv2.circle(image, (int(centre_reprojected[0]), int(centre_reprojected[1])), 3, (0, 255, 0), -1)
+
+        return error_np, error, image
+    
+    return error_np, error
+
+
+def calculate_reprojection_error(mtx, dist, objPoints, imgPoints, image_pths=None, waitTime=1):
+    
+    #faces, number_horizontally, number_vertically, size_chess, aruco_size, reprojection_images_pth,img_ext, charuco_board_save_pth, dictionary,wait_time=1):
+   
+    # 3D points of test board
+    #all3DIDs_np, allCorners3D_np, boards, num_chess_corners = boards_3D_points(faces, number_horizontally, number_vertically, size_chess, aruco_size, dictionary, charuco_board_save_pth=charuco_board_save_pth)
+
+    #image_pths = glob.glob(f'{reprojection_images_pth}/*.{img_ext}')
+    #image_pths, imgPoints, objPoints, img_shape = detect_corners_charuco_cube_images( boards, dictionary,num_chess_corners, allCorners3D_np, all3DIDs_np, image_pths)
+    
+    # update image paths with non-rejected paths
+    #image_pths = [image_pths[i] for i in range(len(image_pths)) if i not in rejected_ids]
+    # Calculate reprojection error on new images
+    mean_errors = []
+    mean_errors_np = []
+    for i in range(len(objPoints)):
+        
+        if len(objPoints[i])<4 or len(imgPoints[i])<4:
+            continue
+        # load image
+        #image = cv2.imread(image_pths[i])
+        #image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        # Estimate rvec and tvec using solvePnP
+        retval, rvec, tvec = cv2.solvePnP(objPoints[i], imgPoints[i], mtx, dist)
+        # Project 3D points to image plane
+        imgpoints_reprojected, _ = cv2.projectPoints(objPoints[i], rvec, tvec, mtx, dist)
+        imgpoints_detected = imgPoints[i]
+        # calculate error
+        if image_pths is not None:
+            image = cv2.imread(image_pths[i])
+            error_np, error, annotated_image = reprojection_error(imgpoints_detected, imgpoints_reprojected, image=image)
+            cv2.imshow('charuco board', annotated_image)
+            cv2.waitKey(waitTime) 
+        else:
+            error_np, error = reprojection_error(imgpoints_detected, imgpoints_reprojected)
+        mean_errors_np.append(error_np)
+        mean_errors.append(error)
+
+
+    
+    reprojection_error_mean_final = np.array(mean_errors).mean()
+    #print(f'Reprojection error: {np.array(mean_errors).mean()}')
+    #print(f'Reprojection error np: {np.array(mean_errors_np).mean()}')
+    #path_name = os.path.basename(reprojection_images_pth).split('_')[-1]
+    #print(f'{path_name} reprojection error: {reprojection_error_mean_final}')
+    return reprojection_error_mean_final
 
 
 
@@ -76,7 +261,11 @@ def detect_corners_charuco_cube_images( board, image_pths, min_num_corners=6, wa
     charuco_corners_3D = board.getChessboardCorners() # allCorners3D_np
     num_chess_corners = len(charuco_corners_3D) # number_of_corners_per_face
     charuco_ids_3D = np.arange(0, num_chess_corners) # all3DIDs_np
-        
+    
+    if min_num_corners is None:
+        min_num_corners = int(0.2*num_chess_corners)
+        if min_num_corners<6:
+            min_num_corners = 6
     # detect corners in images
     for image_pth in image_pths:
 
@@ -112,13 +301,14 @@ def detect_corners_charuco_cube_images( board, image_pths, min_num_corners=6, wa
         _, allCorners3D_np_sorted_filtered = sort_and_filter_matched_corners(charuco_detected_corners.squeeze(), charuco_corners_3D, charuco_detected_ids, charuco_ids_3D)
         objPoints.append(allCorners3D_np_sorted_filtered.reshape(-1,1,3))
 
-    return updated_image_pths, imgPoints, objPoints, image.shape[0:-1]
+    return updated_image_pths, imgPoints, objPoints, min_num_corners #image.shape[0:-1]
 
 
 def calibrate_charuco_board( 
-                           camera_intrinsics_initial_guess='', 
+                           intrinsics_initial_guess_pth='', 
                            calibration_save_pth='',
-                           image_shape = (1280, 720),
+                           image_shape = (1280, 720), 
+                           imgPoints=None, objPoints=None,
                            # if we want to calibrate from images
                            image_pths=[] ,
                            board = None 
@@ -131,21 +321,18 @@ def calibrate_charuco_board(
 
     '''
 
-    # create folder to save calibration results
-    if not os.path.exists(calibration_save_pth):
-        os.makedirs(calibration_save_pth)
+
 
     # all3DIDs_np, allCorners3D_np, boards, number_of_corners_per_face = boards_3D_points(faces, number_horizontally, number_vertically, size_chess, aruco_size, dictionary, charuco_board_save_pth=charuco_board_save_pth)
     if image_pths:
         _, imgPoints, objPoints, image_shape = detect_corners_charuco_cube_images( board, image_pths, waiting_time=0)
-    else:
-        imgPoints, objPoints = [], [], []
+
     # calibrate camera with allCorners and allIds (2D) of all cube charuco faces (allCorners3D, all3DIDs)
     if len(imgPoints) > 0:
         # to perform camera calibration with a non-planar target, we need to 
         # provide a rough initial estimate of the camera calibration parameters
-        if len(camera_intrinsics_initial_guess) > 0:
-            initial_camera_matrix = np.loadtxt(camera_intrinsics_initial_guess)
+        if len(intrinsics_initial_guess_pth) > 0:
+            initial_camera_matrix = np.loadtxt(intrinsics_initial_guess_pth)
         else:
             fx = 420  # Focal length in x direction 800, 640
             fy = 420   # Focal length in y direction 800
@@ -164,17 +351,24 @@ def calibrate_charuco_board(
         ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objPoints, imgPoints,image_shape , initial_camera_matrix, initial_dist_coeffs, flags=flags, criteria=criteria)
         # save results
         if ret:
-            print("Calibration successful")
-            print("Camera matrix:\n", mtx)
-            print("Distortion coefficients:\n", dist)
-            print("Rotation vectors:\n", rvecs)
-            print("Translation vectors:\n", tvecs)
-            # Save the camera calibration result in txt files
-            np.savetxt(f'{calibration_save_pth}/camera_matrix.txt', mtx, delimiter=',')
-            np.savetxt(f'{calibration_save_pth}/dist_coeffs.txt', dist, delimiter=',')
+
+            if len(calibration_save_pth)>0:
+
+                # create folder to save calibration results
+                if not os.path.exists(calibration_save_pth):
+                    os.makedirs(calibration_save_pth)
+                print("Calibration successful")
+                print("Camera matrix:\n", mtx)
+                print("Distortion coefficients:\n", dist)
+                print("Rotation vectors:\n", rvecs)
+                print("Translation vectors:\n", tvecs)
+                # Save the camera calibration result in txt files
+                np.savetxt(f'{calibration_save_pth}/camera_matrix.txt', mtx, delimiter=',')
+                np.savetxt(f'{calibration_save_pth}/dist_coeffs.txt', dist, delimiter=',')
 
 
-    return mtx, dist
+            return mtx, dist
+    return None, None
 
 
 
--- main.py
+++ main.py
@@ -3,33 +3,40 @@ import os
 import random
 import numpy as np
 import pandas as pd
-from charuco_utils import detect_corners_charuco_cube_images, generate_charuco_board
+from charuco_utils import analyse_calibration_data, detect_corners_charuco_cube_images, generate_charuco_board
+import matplotlib.pyplot as plt
 
 
-def generate_board_table(size_chess, camera, image_pths, board):
+def generate_board_table(size_chess, camera, image_pths, board, min_num_corners=6, waiting_time=1):
     """
     Generate a table of the chessboard corners in the world coordinate system.
     """
     
     #_, imgPoints, objPoints, image_shape = detect_corners_charuco_cube_images( board, image_pths)
-    updated_image_pths, imgPoints, objPoints, image_shape = detect_corners_charuco_cube_images( board, image_pths, waiting_time=1)
+    updated_image_pths, imgPoints, objPoints, min_corners = detect_corners_charuco_cube_images( board, image_pths, min_num_corners=min_num_corners, waiting_time=waiting_time)
     # convert updated image paths- split path to get: [image number, pose number, degree number, going forward or backward]
     # convert to pandas dataframe
     data = {'paths':updated_image_pths, 'imgPoints':imgPoints, 'objPoints':objPoints}
     data_df = pd.DataFrame(data=data)
-
+    # adding columns to describe pose, chess size, degree, direction
+    data_df[['chess_size', 'pose', 'deg', 'direction']] = data_df['paths'].str.extract('acc_(\d+)_pos(\d+)_deg(\d+)_(\d+)')
+    # also adding frame number
+    data_df['frame_number'] = data_df['paths'].str.extract('(\d+).png')
+    # convert to integers
+    data_df[["pose", "chess_size", "deg", "direction"]] = data_df[["pose", "chess_size", "deg", "direction"]].apply(pd.to_numeric)
+    
     # save original number of images and the number of images with detected corners aswell as the number of corners detected in total
     original_number_of_images = len(image_pths)
     number_of_images_with_corners = len(updated_image_pths)
     number_of_corners_detected = sum([len(i) for i in imgPoints])
-    titles = ['original_number_of_images', 'number_of_images_with_corners', 'number_of_corners_detected']
-    values = [original_number_of_images, number_of_images_with_corners, number_of_corners_detected]
+    titles = ['original_number_of_images', 'number_of_images_with_corners', 'number_of_corners_detected', 'minimum_corners_required']
+    values = [original_number_of_images, number_of_images_with_corners, number_of_corners_detected, min_corners]
     info = {'titles':titles, 'data':values}
     info_df = pd.DataFrame(data=info)
     
-    
-    data_df.to_pickle(f'results/{size_chess}_{camera}_data.pkl')
-    info_df.to_csv(f'results/{size_chess}_{camera}_info.csv')
+
+    data_df.to_pickle(f'results/corner_data/{size_chess}_{camera}_corner_data.pkl')
+    info_df.to_csv(f'results/corner_data/{size_chess}_{camera}_corner_info.csv')
 
     return data_df, info_df
 
@@ -37,28 +44,105 @@ def generate_board_table(size_chess, camera, image_pths, board):
     # generate 11 random numbers that are in the range of the above loaded images
     #random_indices = random.sample(range(len(path_to_endo_images)), R+n)
 
+def perform_analysis(camera, size_chess, data_df, reprojection_data_df, repeats=1000, num_images_start=5, num_images_end=60, num_images_step=2, 
+                     visualise_reprojection_error=False, waitTime=1 ): #, info_df,board,image_pths
+    
+    # perform calibration analysis
+    if camera =='realsense':
+        intrinsics_initial_guess_pth = f'calibration_estimates/intrinsics_realsense.txt'
+    else:
+        intrinsics_initial_guess_pth = f'calibration_estimates/intrinsics_endo.txt'
+
+    num_images_lst = np.arange(num_images_start, num_images_end, num_images_step)
+    average_error_lst = []
+    error_lst = []
+    all_intrinsics = []
+    all_distortion = []
+    std_error_lst = []
+    for num_images in num_images_lst:
+        errors, intrinsics, distortion = analyse_calibration_data(data_df,
+                             reprojection_data_df, # number of frames to use for calculating reprojection loss
+                             n = num_images, # number of frames to use for calibration
+                             repeats = repeats, # number of repeats for the calibration
+                             #size_chess = size_chess,
+                             #calibration_save_pth = '',
+                             intrinsics_initial_guess_pth=intrinsics_initial_guess_pth,
+                             visualise_reprojection_error=visualise_reprojection_error,
+                             waitTime=waitTime # time to display each image for (in seconds) when showing reprojection
+                             )
+        # ignore infinite errors
+        errors = [e for e in errors if not np.isinf(e)]
+        # ignore anything larger than 20
+        errors = [e for e in errors if e < 20]
+        error_lst.append(errors)
+        average_error_lst.append(np.mean(errors))
+        std_error_lst.append(np.std(errors))
+        all_intrinsics.append(intrinsics)
+        all_distortion.append(distortion)
+
+    # plot number of images vs average reprojection error with standard deviation
+    plt.plot(num_images_lst, average_error_lst, label=f'{size_chess}_{camera}')
+    plt.errorbar(num_images_lst, average_error_lst, yerr=std_error_lst)
+    plt.xlabel('Number of images')
+    plt.ylabel('Average reprojection error')
+    
+
+    # save intrinsics, distortion and errors
+    data = {'errros':error_lst, 'intrinsics':all_intrinsics,'distortion':all_distortion,  'average_error':average_error_lst, 'std_error':std_error_lst}
+    data_df = pd.DataFrame(data=data)
+    data_df.to_pickle(f'results/calibration_error_data/{size_chess}_{camera}_calibration_data.pkl')
+
+
+
 def main(): 
     data_path = '/Users/aure/Documents/CARES/data/massive_calibration_data'
     #analyse_calibration_data(data_path, img_ext='png')
     img_ext = 'png'
-    size_chess = 15
-    camera = 'realsense' # realsense
-    image_pths = glob.glob(f'{data_path}/{size_chess}_charuco/pose0/acc_{size_chess}_pos*_deg*_*/raw/he_calibration_images/hand_eye_{camera}/*.{img_ext}')
-    board= generate_charuco_board(size_chess)
+    #size_chess = 30
+    #camera = 'endo' # realsense/endo
     
+
+    chess_sizes = [15, 20, 25, 30] #15, 20, 25, 
+    cameras = ['endo', 'realsense'] # 'endo', 'realsense'
+    for camera in cameras:
+        plt.figure()
+        for size_chess in chess_sizes:
+            print(f'camera {camera}, size_chess {size_chess}')
+            image_pths = glob.glob(f'{data_path}/{size_chess}_charuco/pose*/acc_{size_chess}_pos*_deg*_*/raw/he_calibration_images/hand_eye_{camera}/*.{img_ext}')
+            board= generate_charuco_board(size_chess)
+        
+            # generate the board data by detecting the corners in the images or load previously generated data
+            if f'{size_chess}_{camera}_corner_data_calibration_dataset.pkl' in os.listdir('results/corner_data/filtered_data'):
+                data_df = pd.read_pickle(f'results/corner_data/filtered_data/{size_chess}_{camera}_corner_data_calibration_dataset.pkl')
+                reprojection_data_df = pd.read_pickle(f'results/corner_data/filtered_data/{size_chess}_{camera}_corner_data_reprojection_dataset.pkl')
+                info_df = pd.read_csv(f'results/corner_data/{size_chess}_{camera}_corner_info.csv')
+            else:
+                data_df, info_df = generate_board_table(size_chess, camera, image_pths, board, min_num_corners=None, waiting_time=1)
+    
+            # perform calibration analysis
+            perform_analysis(camera, size_chess, 
+                             data_df,reprojection_data_df, repeats=1000, 
+                             num_images_start=5, num_images_end=50, num_images_step=2,
+                             visualise_reprojection_error=False, waitTime=1)
+        
+        
+        plt.legend()
+        plt.savefig(f'results/calibration_error_data/calibration_error_{camera}.png')
     # check if f'results/{size_chess}_{camera}_data.pkl' in os.listdir('results')
     # if not, generate the table and save it
     # if yes, load the table and continue
 
-
-    if f'{size_chess}_{camera}_data.pkl' in os.listdir('results'):
-        data_df = pd.read_pickle(f'results/{size_chess}_{camera}_data.pkl')
-        info_df = pd.read_csv(f'results/{size_chess}_{camera}_info.csv')
-    else:
-        data_df, info_df = generate_board_table(size_chess, camera, image_pths, board)
+    ################################ DETECT CORNERS IN ALL FRAMES
     
-    # perform calibration analysis
 
+    """ errors, intrinsics, distortion = analyse_calibration_data(data_df, 
+                             R = 50, # number of frames to use for calculating reprojection loss
+                             n = 10, # number of frames to use for calibration
+                             repeats = 10, # number of repeats for the calibration
+                             size_chess = size_chess, 
+                             calibration_save_pth = '',
+                             intrinsics_initial_guess_pth=intrinsics_initial_guess_pth,
+                             ) """
 
 
     #np.savetxt(f'results/{size_chess}_info.txt', ['original_number_of_images', original_number_of_images, number_of_images_with_corners, number_of_corners_detected])
